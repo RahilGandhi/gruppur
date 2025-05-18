6aadbcf5c54b671ca86133bec67f6715696fb341
Rahil Gandhi <rahilhitesh2002@gmail.com>
2025-05-18 16:27:04 +0530

    API URL Changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://grupp-backend.onrender.com",
   withCredentials: true,  // Allows sending cookies
 });
 
